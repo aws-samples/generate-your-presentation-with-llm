2b8312c2cda4ff79c50770f31185c6c2020b33df
Luca Perrozzi <lperroz@amazon.ch>
2025-07-07 13:55:01 +0000

    fix check pwd

--- pptx-generator.py
+++ pptx-generator.py
@@ -87,9 +87,9 @@ st.markdown(
                 """, unsafe_allow_html=True
             )
 
-# if not check_password("PPTX generator App"):
-    # # need to login first
-    # st.stop()
+if not check_password("PPTX generator App"):
+    # need to login first
+    st.stop()
 
 # MAIN PAGE        
 st.title('Generate or translate your presentation with Amazon Bedrock!')
